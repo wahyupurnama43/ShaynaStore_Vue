1daefdf30d512c3c0cc3e1932e381936aa1f743d
wahyupurnama43 <wahyupurnama434@gmail.com>
2021-08-08 21:24:45 +0800

    first upload

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroHeader />
+    <women-banner />
+    <Instagram />
+    <patner />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroHeader from "@/components/HeroHeader.vue";
+import WomenBanner from "@/components/WomenBanner.vue";
+import Instagram from "@/components/Instagram.vue";
+import Patner from "@/components/Patner.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    HeroHeader,
+    WomenBanner,
+    Instagram,
+    Patner,
+    FooterShayna,
+  },
+};
+</script>
